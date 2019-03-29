1ea57c30f232feb0517545e65de46f9b7583cdf9
nvtuluva <nvtuluva@sysgain.com>
2019-03-29 11:06:04 +0530

    Update test.sh

--- test.sh
+++ test.sh
@@ -17,3 +17,21 @@ ADMIN_SITE_PORT=${16};
 CAKESHOP_FQDN=${17};
 CAKESHOP_STARTING_PORT=${18};
 echo "=============================================================" >> /tmp/viveklog
+echo "AZUREUSER is $AZUREUSER" >> /tmp/viveklog
+echo "PASSWD is $PASSWD" >> /tmp/viveklog
+echo "PASSPHRASE is $PASSPHRASE" >> /tmp/viveklog
+echo "ARTIFACTS_URL_PREFIX is $ARTIFACTS_URL_PREFIX" >> /tmp/viveklog
+echo "NETWORK_ID is $NETWORK_ID" >> /tmp/viveklog
+echo "MAX_PEERS is $MAX_PEERS" >> /tmp/viveklog
+echo "NODE_TYPE is $NODE_TYPE" >> /tmp/viveklog
+echo "GETH_IPC_PORT is $GETH_IPC_PORT" >> /tmp/viveklog
+echo "NUM_VOTERS is $NUM_VOTERS" >> /tmp/viveklog
+echo "VN_NODE_PREFIX is $VN_NODE_PREFIX" >> /tmp/viveklog
+echo "NUM_BLOCKMAKERS is $NUM_BLOCKMAKERS" >> /tmp/viveklog
+echo "BM_NODE_PREFIX is $BM_NODE_PREFIX" >> /tmp/viveklog
+echo "NUM_OBSERVERS is $NUM_OBSERVERS" >> /tmp/viveklog
+echo "OB_NODE_PREFIX is $OB_NODE_PREFIX" >> /tmp/viveklog
+echo "NODE_SEQNUM is $NODE_SEQNUM" >> /tmp/viveklog
+echo "ADMIN_SITE_PORT is $ADMIN_SITE_PORT" >> /tmp/viveklog
+echo "CAKESHOP_FQDN is $CAKESHOP_FQDN" >> /tmp/viveklog
+echo "CAKESHOP_STARTING_PORT is $CAKESHOP_STARTING_PORT" >> /tmp/viveklog
